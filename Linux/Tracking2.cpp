0acb3f6bd951cbeef70ec689a81963edc8b4676c
Vi Vuong <vuongkyvi@gmail.com>
2017-11-14 03:57:25 +0000

    Updated program and blobs parameters

---
+++ Linux/Tracking2.cpp
@@ -0,0 +1,15 @@
+// Tracking2.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <opencv2\opencv.hpp>
+using namespace cv;
+
+
+
+int main()
+{
+	
+    return 0;
+}
+
---
+++ Linux/bucket.h
@@ -0,0 +1,28 @@
+//Contours are useless and CPU demanding
+
+
+#pragma once
+#include "objectDetection.h"
+
+
+class bucket :
+	public objectDetection
+{
+private:
+	std::vector<KeyPoint> Keypoints;
+	std::string intToString(int number);
+	Scalar low_threshold, high_threshold;
+	virtual bool detectContours(Mat frame, std::vector<std::vector<Point>> &contours);
+	virtual Mat colorFilter(Mat frame, std::string arg = "contours");
+	bool poly(std::vector<std::vector<Point>>);
+	bool filterContourArea(std::vector<std::vector<Point>>& contours, double limit);
+	bool filterRecArea(std::vector<Rect>& rects, double limit);
+protected:
+	std::vector<std::vector<Point>> contours;
+public:
+	bucket(Mat frame, Scalar low_threshold, Scalar high_threshold);
+	virtual Scalar detect();
+	void blobDetect();
+	~bucket();
+};
+
