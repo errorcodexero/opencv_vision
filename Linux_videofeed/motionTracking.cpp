f783ea16b50f160215f6c757aeb39d1fb31cc2f1
Vi Vuong <vuongkyvi@gmail.com>
2017-11-23 00:33:54 -0800

    Updated

--- Linux_videofeed/motionTracking.cpp
+++ Linux_videofeed/motionTracking.cpp
@@ -3,13 +3,13 @@
 //Written by  Kyle Hounslow, January 2014
 
 //Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software")
-//, to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, 
+//, to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense,
 //and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
 
 //The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 
 //THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-//FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
+//FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
 //LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
 //IN THE SOFTWARE.
 
@@ -50,55 +50,10 @@ string intToString(int number){
 	return ss.str();
 }
 
-/*void searchForMovement(Mat thresholdImage, Mat &cameraFeed){
-	//notice how we use the '&' operator for the cameraFeed. This is because we wish
-	//to take the values passed into the function and manipulate them, rather than just working with a copy.
-	//eg. we draw to the cameraFeed in this function which is then displayed in the main() function.
-	bool objectDetected=false;
-	Mat temp;
-	thresholdImage.copyTo(temp);
-	//these two vectors needed for output of findContours
-	vector< vector<Point> > contours;
-	vector<Vec4i> hierarchy;
-	//find contours of filtered image using openCV findContours function
-	//findContours(temp,contours,hierarchy,CV_RETR_CCOMP,CV_CHAIN_APPROX_SIMPLE );// retrieves all contours
-	findContours(temp,contours,hierarchy,CV_RETR_EXTERNAL,CV_CHAIN_APPROX_SIMPLE );// retrieves external contours
 
-	//if contours vector is not empty, we have found some objects
-	if(contours.size()>0)objectDetected=true;
-	else objectDetected = false;
-
-	if(objectDetected){
-		//the largest contour is found at the end of the contours vector
-		//we will simply assume that the biggest contour is the object we are looking for.
-		vector< vector<Point> > largestContourVec;
-		largestContourVec.push_back(contours.at(contours.size()-1));
-		//make a bounding rectangle around the largest contour then find its centroid
-		//this will be the object's final estimated position.
-		objectBoundingRectangle = boundingRect(largestContourVec.at(0));
-		int xpos = objectBoundingRectangle.x+objectBoundingRectangle.width/2;
-		int ypos = objectBoundingRectangle.y+objectBoundingRectangle.height/2;
-
-		//update the objects positions by changing the 'theObject' array values
-		theObject[0] = xpos , theObject[1] = ypos;
-	}
-	//make some temp x and y variables so we dont have to type out so much
-	int x = theObject[0];
-	int y = theObject[1];
-	//draw some crosshairs on the object
-	circle(cameraFeed,Point(x,y),20,Scalar(0,255,0),2);
-	line(cameraFeed,Point(x,y),Point(x,y-25),Scalar(0,255,0),2);
-	line(cameraFeed,Point(x,y),Point(x,y+25),Scalar(0,255,0),2);
-	line(cameraFeed,Point(x,y),Point(x-25,y),Scalar(0,255,0),2);
-	line(cameraFeed,Point(x,y),Point(x+25,y),Scalar(0,255,0),2);
-	putText(cameraFeed,"Tracking object at (" + intToString(x)+","+intToString(y)+")",Point(x,y),1,1,Scalar(255,0,0),2);
-
-
-
-}*/
 int main(int ac, char **av){
-	//ac--;
-	//av++;
+	ac--;
+	av++;
 
 	//some boolean variables for added functionality
 	bool objectDetected = false;
@@ -109,10 +64,10 @@ int main(int ac, char **av){
 	bool pause = false;
 	//set up the matrices that we will need
 	//the two frames we will be comparing
-	Mat frame1,frame2;
+	Mat frame1;
 	//their grayscale images (needed for absdiff() function)
 	// open capture
-int descriptor = v4l2_open("/dev/video0", O_RDWR);
+/*int descriptor = v4l2_open("/dev/video0", O_RDWR);
 	v4l2_control c;
 c.id = V4L2_CID_EXPOSURE_AUTO_PRIORITY;
 c.value = 3;
@@ -134,7 +89,7 @@ c.id = V4L2_CID_EXPOSURE_ABSOLUTE;
 c.value = 30;
 if(v4l2_ioctl(descriptor, VIDIOC_S_CTRL, &c) == 0)
     cout << "success";
-	//video capture object.
+	//video capture object.*/
 	VideoCapture capture(0);
 	//capture.set(CAP_PROP_BRIGHTNESS,0.1);
 		if(!capture.isOpened()){
@@ -143,20 +98,20 @@ if(v4l2_ioctl(descriptor, VIDIOC_S_CTRL, &c) == 0)
 			return -1;
 		}
 
-	
+
 
 	while(1){
 
 		//we can loop the video by re-opening the capture every time the video reaches its last frame
-		//capture.open(*av);
+		capture.open(*av);
 
 		//check if the video has reach its last frame.
 		//we add '-1' because we are reading two frames from the video at a time.
 		//if this is not included, we get a memory error!
-		
+
 
 			//read first frame
-			//capture.read(frame1); 
+			//capture.read(frame1);
 			capture >> frame1;
 			/*cvSetImageROI(frame1, cvRect(0, 240, 640, 240));
 			Mat frame = cvCreateImage(cvGetSize(frame1), frame1.depth, frame1.channels);
@@ -168,13 +123,11 @@ if(v4l2_ioctl(descriptor, VIDIOC_S_CTRL, &c) == 0)
 			imshow("frame", frame);
 			bucket b(frame,low,high);
 			//b.showContours();
-			b.blobDetect();
+
 			if(debugMode==true){
-				//show the difference image and threshold image
-				//imshow("Dif", differenceImage);
-				//imshow("Thresh", thresholdImage);
+				b.blobDetect();
 				//b.blobDetect();
-				
+
 			}else{
 				//if not in debug mode, destroy the windows so we don't see them anymore
 				//destroyWindow("Dif");
@@ -200,7 +153,7 @@ if(v4l2_ioctl(descriptor, VIDIOC_S_CTRL, &c) == 0)
 			imshow("Frame1",frame);
 			//check to see if a button has been pressed.
 			//this 10ms delay is necessary for proper operation of this program
-			//if removed, frames will not have enough time to referesh and a blank 
+			//if removed, frames will not have enough time to referesh and a blank
 			//image will appear.
 			switch(waitKey(10)){
 
@@ -220,10 +173,10 @@ if(v4l2_ioctl(descriptor, VIDIOC_S_CTRL, &c) == 0)
 				pause = !pause;
 				if(pause == true){ cout<<"Code paused, press 'p' again to resume"<<endl;
 				while (pause == true){
-					//stay in this loop until 
+					//stay in this loop until
 					switch (waitKey()){
 						//a switch statement inside a switch statement? Mind blown.
-					case 112: 
+					case 112:
 						//change pause back to false
 						pause = false;
 						cout<<"Code resumed."<<endl;
@@ -236,9 +189,9 @@ if(v4l2_ioctl(descriptor, VIDIOC_S_CTRL, &c) == 0)
 			}
 
 
-		
+
 		//release the capture before re-opening and looping again.
-		
+
 	}
 	capture.release();
 	return 0;
