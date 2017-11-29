782144762fe5cb48dbf7fe9d43c402e67a5b74e7
Vi Vuong <vuongkyvi@gmail.com>
2017-11-28 20:04:11 -0800

    Final code added

---
+++ Notrelease/bucket.cpp
@@ -0,0 +1,283 @@
+//#include "stdafx.h"
+#include "bucket.h"
+#include <vector>
+
+Scalar hsvlow(0, 0, 150), hsvhigh(180, 30, 230);
+
+std::string bucket::intToString(int number) {
+
+	//this function has a number input and string output
+	std::stringstream ss;
+	ss << number;
+	return ss.str();
+}
+
+bucket::bucket(Mat frame, Scalar low_threshold, Scalar high_threshold)
+{
+	this->frame = frame;
+	this->frame_original = frame;
+	this->low_threshold = low_threshold;
+	this->high_threshold = high_threshold;
+}
+
+
+/*Mat bucket::yuvEqualize(Mat frame) {
+	cvtColor(frame,frame,CV_BGR2YUV);
+	std::vector<Mat> channels;
+	split(frame,channels);
+	equalizeHist(channels[0],channels[0]);
+	merge(channels, frame);
+	cvtColor(frame,frame,CV_YUV2BGR);
+	imshow("YUV", frame);
+	return frame;
+}*/
+
+Mat bucket::colorFilter(Mat frame, std::string arg ) {
+	if (arg == "contours") {
+		Mat hsv_frame;
+		cvtColor(frame, hsv_frame, COLOR_RGB2HSV);
+		Mat mask, filtered;
+		inRange(hsv_frame, hsvlow, hsvhigh, mask);
+		bitwise_and(frame, frame, filtered, mask);
+		//cvtColor(filtered, filtered, COLOR_BGR2GRAY);
+		imshow("hsv", filtered);
+		return filtered;
+	}
+	else if (arg == "blobs") {
+		Mat hsv_frame;
+		cvtColor(frame, hsv_frame, COLOR_RGB2HSV);
+		Mat mask, filtered;
+		inRange(hsv_frame, hsvlow, hsvhigh, mask);
+		bitwise_and(frame, frame, filtered, mask);
+		//std::cout << filtered.type() << std::endl;
+		return filtered;
+	}
+	else if (arg=="gray") {
+		Mat gray, thresholded;
+		cvtColor(frame,gray,COLOR_BGR2GRAY);
+		imshow("gray", gray);
+		Mat mask;
+		Mat filtered(frame.rows, frame.cols, frame.type());
+		inRange(gray, low_threshold, high_threshold, mask);
+		bitwise_and(gray, gray, filtered, mask);
+		//cvtColor(frame,frame,COLOR_BGR2GRAY);
+		imshow("f",filtered);
+		return filtered;
+
+	}
+	else return Mat();
+}
+
+
+
+bool bucket::detectContours(Mat frame, std::vector<std::vector<Point>> &contours)
+{
+	frame = colorFilter(frame_original,"gray");
+	threshold(frame,frame,200,255,3);
+	imshow("Thresholded", frame);
+	std::vector<Vec4i> hierachy;
+	Mat canny_output;
+	blur(frame, frame, Size(5,5));
+	Canny(frame, canny_output, 25, 75);
+	findContours(canny_output, contours, hierachy, CV_RETR_EXTERNAL, CHAIN_APPROX_SIMPLE);
+	//filterContourArea(contours, 200);
+	Mat drawing = Mat::zeros(canny_output.size(), CV_8UC3);
+	drawContours(drawing, contours, -1, Scalar(255,255,255),1,8,hierachy,1);
+	imshow("contours", drawing);
+	return true;
+}
+
+
+std::vector<Rect> bucket::poly(Mat frame, std::vector<std::vector<Point>> contours)
+{
+	//std::vector<std::vector<Point>>::iterator i = contours.begin();
+	//std::vector<std::vector<Point>> derivedPolygon;
+	auto derivedPolygon = contours;
+	std::vector<Rect> rectangles;
+	Mat rects = Mat::zeros(Size(frame.cols, frame.rows), CV_8U);
+	for (size_t i = 0; i < contours.size(); i++) {
+		double epsilon = 0.1 * arcLength(contours[i], true);
+		approxPolyDP(contours[i], derivedPolygon[i], epsilon, true);
+		rectangles.push_back(boundingRect(derivedPolygon[i]));
+
+	}
+	Mat poly = Mat::zeros(Size(frame.cols,frame.rows),frame.type());
+	//drawContours(poly, derivedPolygon, -1,255);
+
+	//std::cout << rectangles.size() << std::endl;
+	//Merge overlapped
+	bool changed = true;
+	while(changed) {
+		std::vector<Rect> orig = rectangles;
+		rectangles.clear();
+		changed = false;
+		for (size_t i = 0; i< orig.size(); i++) {
+			Rect r = orig[i];
+			size_t j = i+1;
+			while (j<orig.size()) {
+				Rect intersect = r & orig[j];
+				if(intersect.width>0 && intersect.height>0) {
+					r = r | orig[j];
+					orig.erase(orig.begin()+j);
+					changed = true;
+
+				}
+				else j++;
+			}
+			if ((r.area()>400) && (r.width/r.height < 0.8)) rectangles.push_back(r);
+		}
+
+	}
+	//std::cout << rectangles.size() << std::endl;
+	/*for (unsigned int i =0; i< rectangles.size(); i++) {
+		rectangle(rects, rectangles[i], Scalar(255, 255, 255));
+	}*/
+	//imshow("rects", rects);
+	//imshow("poly", poly);
+	return rectangles;
+}
+
+bool bucket::filterContourArea(std::vector<std::vector<Point>>& contours, double limit)			//Not Working
+{
+	for (unsigned int i = 0; i < contours.size(); i++) {
+		if (contourArea(contours[i]) < limit) contours.erase(contours.begin() + i);
+		//if (contours[i].size() < limit) contours.erase(contours.begin() + i);
+	};
+	Mat drawing = Mat::zeros(frame_original.size(), frame_original.type());
+	drawContours(drawing, contours, -1, Scalar(255, 255, 255));
+	//imshow("filtered contours", drawing);
+	return true;
+}
+
+bool bucket::filterRecArea(std::vector<Rect>& rects, double limit)
+{
+	for (size_t i = 0; i < rects.size(); i++) {
+		if (rects[i].area() < limit) rects.erase(rects.begin() + i);
+	};
+	return true;
+}
+
+int bucket::blobDetect()
+{
+	//Mat blob=colorFilter(frame,"gray");
+	//Mat blob = frame;
+	//imshow("blobs", blob);
+	//Mat hsv_frame = colorFilter(frame_original, "contours");
+	Mat blob = colorFilter(frame_original, "gray");
+	//Mat blob = hsv_frame;
+	SimpleBlobDetector::Params params;
+
+
+	//Color
+	params.blobColor = 255;
+
+	// Change thresholds
+	params.minThreshold = 130;
+	params.maxThreshold = 200;
+	params.thresholdStep = 10;
+
+	// Filter by Area.
+	params.filterByArea = true;
+	params.minArea = 1500;
+	params.maxArea = 90000;
+
+	params.filterByCircularity = false;
+	params.filterByConvexity = false;
+	params.filterByInertia = false;
+
+
+	// Detect blobs.
+	//std::vector<KeyPoint> keypoints;
+	Ptr<SimpleBlobDetector> detector = SimpleBlobDetector::create(params);
+	detector->detect(blob, Keypoints);
+
+
+
+	// Draw detected blobs as red circles.
+	// DrawMatchesFlags::DRAW_RICH_KEYPOINTS flag ensures the size of the circle corresponds to the size of blob
+	Mat im_with_keypoints;
+	drawKeypoints(blob, Keypoints, im_with_keypoints, Scalar(0, 0, 255), DrawMatchesFlags::DRAW_RICH_KEYPOINTS);
+
+	// Show blobs
+	imshow("keypoints", im_with_keypoints);
+	std::vector<Vec4i> hierachy;
+	Mat canny_output;
+	std::vector<Point> Targets;
+	Canny(blob, canny_output, 25, 75);
+	findContours(canny_output, contours, hierachy, CV_RETR_EXTERNAL, CHAIN_APPROX_SIMPLE);
+	//filterContourArea(contours, 0);
+	//poly(contours);
+	//Mat drawing = Mat::zeros(canny_output.size(), CV_8UC3);
+	//drawContours(drawing, contours, -1, Scalar(255,255,255),1,8,hierachy,1);
+	//imshow("contours", drawing);
+	//filterContourArea(contours, 200);
+	std::vector<Rect> rectangles = poly(frame, contours);
+	Point pmin(0,0);
+	for (unsigned int i = 0; i < Keypoints.size(); i++) {
+		Point p = Keypoints[i].pt;
+		for(unsigned int j = 0;j<rectangles.size();j++) {
+			Rect r = rectangles[j];
+			if(p.inside(r))
+			if (p.y>pmin.y) pmin = p;
+			}
+		}
+	circle(frame, pmin, 20, Scalar(0, 255, 0), 2);
+	line(frame, pmin, Point(pmin.x, pmin.y + 25), Scalar(0, 255, 0), 2);
+	line(frame, pmin, Point(pmin.x, pmin.y - 25), Scalar(0, 255, 0), 2);
+	line(frame, pmin, Point(pmin.x - 25, pmin.y), Scalar(0, 255, 0), 2);
+	line(frame, pmin, Point(pmin.x + 25, pmin.y), Scalar(0, 255, 0), 2);
+	putText(frame, "Tracking object at (" + intToString(pmin.x) + "," + intToString(pmin.y) + ")", pmin, 1, 1, Scalar(255, 0, 0), 2);
+	//imshow("keypoints", frame);
+	//return Scalar();
+	return xError(pmin);
+}
+
+
+int xError(Point p) {
+	int r;
+	if (p.x<200) r = p.x-200;
+	else if (p.x>280) r = p.x-280;
+	else r =0;
+	return r;
+}
+
+Scalar bucket::detect()
+{
+	colorFilter(frame);
+	imshow("filtered", frame);
+	detectContours(frame,contours);
+	filterContourArea(contours, 500);
+	//poly(contours);
+	return Scalar(0,0);
+}
+
+/*void bucket::showContours() {
+	/*Mat gray_frame,frame;
+	gray_frame = colorFilter(frame_original,"gray");
+	threshold(gray_frame,frame,200,255,3);
+	imshow("Thresholded",frame);
+	//Mat frame_gray = colorFilter(frame_original,"gray");
+
+	//Mat frame = colorFilter(frame_gray, "contours");
+	Mat yuv_eq = yuvEqualize(frame_original);
+	//Mat hsv_frame = colorFilter(frame_original, "contours");
+	//Mat frame = colorFilter(hsv_frame, "gray");
+	Mat hsv_frame = colorFilter(yuv_eq, "contours");
+	Mat frame = colorFilter(hsv_frame, "gray");
+	std::vector<Vec4i> hierachy;
+	Mat canny_output;
+	//blur(frame, frame, Size(5,5));
+	Canny(frame, canny_output, 25, 75);
+	findContours(canny_output, contours, hierachy, CV_RETR_EXTERNAL, CHAIN_APPROX_SIMPLE);
+	filterContourArea(contours, 0);
+	//poly(contours);
+	Mat drawing = Mat::zeros(canny_output.size(), CV_8UC3);
+	drawContours(drawing, contours, -1, Scalar(255,255,255),1,8,hierachy,1);
+	//imshow("contours", drawing);
+	//filterContourArea(contours, 200);
+	poly(frame, contours);
+}*/
+
+bucket::~bucket()
+{
+}
---
+++ Notrelease/motionTracking.cpp
@@ -0,0 +1,198 @@
+//motionTracking.cpp
+
+//Written by  Kyle Hounslow, January 2014
+
+//Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software")
+//, to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense,
+//and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+//The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+//THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+//IN THE SOFTWARE.
+
+//#include "stdafx.h"
+#include <stdio.h>
+#include <opencv2/opencv.hpp>
+#include <opencv2/highgui.hpp>
+/*#include <libv4l2.h>
+#include <linux/videodev2.h>
+#include <sys/ioctl.h>
+#include <fcntl.h>*/
+#include "bucket.h"
+
+using namespace std;
+using namespace cv;
+
+//our sensitivity value to be used in the threshold() function
+const static int SENSITIVITY_VALUE = 20;
+//size of blur used to smooth the image to remove possible noise and
+//increase the size of the object we are trying to track. (Much like dilate and erode)
+const static int BLUR_SIZE = 10;
+//we'll have just one object to search for
+//and keep track of its position.
+int theObject[2] = {0,0};
+//bounding rectangle of the object, we will use the center of this as its position.
+Rect objectBoundingRectangle = Rect(0,0,0,0);
+
+//Scalar low(0, 0, 170), high(180, 80, 255);
+Scalar low(150), high(230);
+
+
+//int to string helper function
+string intToString(int number){
+
+	//this function has a number input and string output
+	std::stringstream ss;
+	ss << number;
+	return ss.str();
+}
+
+
+int main(int ac, char **av){
+	ac--;
+	av++;
+
+	//some boolean variables for added functionality
+	bool objectDetected = false;
+	//these two can be toggled by pressing 'd' or 't'
+	bool debugMode = false;
+	bool trackingEnabled = false;
+	//pause and resume code
+	bool pause = false;
+	//set up the matrices that we will need
+	//the two frames we will be comparing
+	Mat frame1;
+	//their grayscale images (needed for absdiff() function)
+	// open capture
+int descriptor = v4l2_open("/dev/video0", O_RDWR);
+	v4l2_control c;
+c.id = V4L2_CID_EXPOSURE_AUTO_PRIORITY;
+c.value = 3;
+if(v4l2_ioctl(descriptor, VIDIOC_S_CTRL, &c) == 0)
+    cout << "success";
+c.id = V4L2_CID_EXPOSURE_AUTO;
+c.value = V4L2_EXPOSURE_MANUAL;
+if(v4l2_ioctl(descriptor, VIDIOC_S_CTRL, &c) == 0)
+    cout << "success";
+
+// manual brigctness control
+c.id = V4L2_CID_BRIGHTNESS;
+c.value = 100;
+if(v4l2_ioctl(descriptor, VIDIOC_S_CTRL, &c) == 0)
+{
+    cout << c.value << " success";
+}
+c.id = V4L2_CID_EXPOSURE_ABSOLUTE;
+c.value = 30;
+if(v4l2_ioctl(descriptor, VIDIOC_S_CTRL, &c) == 0)
+    cout << "success";
+	//video capture object.
+	VideoCapture capture(0);
+	//capture.set(CAP_PROP_BRIGHTNESS,0.1);
+		capture.open(*av);
+
+
+
+	while(1){
+
+		//we can loop the video by re-opening the capture every time the video reaches its last frame
+
+		if(!capture.isOpened()){
+			cout<<"ERROR ACQUIRING VIDEO FEED\n";
+			getchar();
+			return -1;
+		}
+		//check if the video has reach its last frame.
+		//we add '-1' because we are reading two frames from the video at a time.
+		//if this is not included, we get a memory error!
+
+			//read first frame
+			//capture.read(frame1);
+			capture >> frame1;
+			/*cvSetImageROI(frame1, cvRect(0, 240, 640, 240));
+			Mat frame = cvCreateImage(cvGetSize(frame1), frame1.depth, frame1.channels);
+			cvCopy(frame1, frame, NULL);
+			cvResetImageROI(frame1);*/
+			/*Rect cropWindow(0, 240, 640, 240);
+			Mat frame;
+			frame1(cropWindow).copyTo(frame);
+			imshow("frame", frame);*/
+			bucket b(frame1,low,high);
+			//b.showContours();
+
+			if(debugMode==true){
+				b.blobDetect();
+				//b.blobDetect();
+
+			}else{
+				//if not in debug mode, destroy the windows so we don't see them anymore
+				//destroyWindow("Dif");
+				//destroyWindow("Thresh");
+			}
+			//use blur() to smooth the image, remove possible noise and
+			//increase the size of the object we are trying to track. (Much like dilate and erode)
+
+			//threshold again to obtain binary image from blur output
+
+			if(debugMode==true){
+				//show the threshold image after it's been "blurred"
+
+			}
+			else {
+				//if not in debug mode, destroy the windows so we don't see them anymore
+
+			}
+
+			//if tracking enabled, search for contours in our thresholded image
+
+			//show our captured frame
+			imshow("Frame1",frame1);
+			//check to see if a button has been pressed.
+			//this 10ms delay is necessary for proper operation of this program
+			//if removed, frames will not have enough time to referesh and a blank
+			//image will appear.
+			switch(waitKey(10)){
+
+			case 27: //'esc' key has been pressed, exit program.
+				return 0;
+			case 116: //'t' has been pressed. this will toggle tracking
+				trackingEnabled = !trackingEnabled;
+				if(trackingEnabled == false) cout<<"Tracking disabled."<<endl;
+				else cout<<"Tracking enabled."<<endl;
+				break;
+			case 100: //'d' has been pressed. this will debug mode
+				debugMode = !debugMode;
+				if(debugMode == false) cout<<"Debug mode disabled."<<endl;
+				else cout<<"Debug mode enabled."<<endl;
+				break;
+			case 112: //'p' has been pressed. this will pause/resume the code.
+				pause = !pause;
+				if(pause == true){ cout<<"Code paused, press 'p' again to resume"<<endl;
+				while (pause == true){
+					//stay in this loop until
+					switch (waitKey()){
+						//a switch statement inside a switch statement? Mind blown.
+					case 112:
+						//change pause back to false
+						pause = false;
+						cout<<"Code resumed."<<endl;
+						break;
+					}
+				}
+				}
+
+
+			}
+
+		}
+
+		//release the capture before re-opening and looping again.
+
+	}
+	capture.release();
+	return 0;
+
+}
